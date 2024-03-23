a480475aec17a03aa7364f236d002529144363cc
lingreading <wouldlikesomebanter@gmail.com>
2024-03-24 04:52:12 +0800

    Add dummy ref to display differing tables

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,11 +1,20 @@
 <script setup>
 import PatientAppointmentTable from "@/components/tables/PatientAppointmentTable.vue";
 import DoctorAppointmentTable from "@/components/tables/DoctorAppointmentTable.vue";
+import { ref } from "vue";
+const userRole = ref("student");
 </script>
 
 <template>
-  <PatientAppointmentTable />
-  <DoctorAppointmentTable />
+  <div>
+    <v-container v-if="userRole === 'student'">
+      <PatientAppointmentTable />
+    </v-container>
+
+    <v-container v-else-if="userRole === 'doctor'"
+      ><DoctorAppointmentTable
+    /></v-container>
+  </div>
 </template>
 
 <style scoped></style>
