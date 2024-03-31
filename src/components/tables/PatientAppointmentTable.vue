b6cf7cbe85304f9fdf79013d347e84c189b6339e
YZDavid <85034436+YZDavid@users.noreply.github.com>
2024-04-01 04:35:59 +0800

    add hardcoded call to database

--- src/components/tables/PatientAppointmentTable.vue
+++ src/components/tables/PatientAppointmentTable.vue
@@ -13,22 +13,29 @@
 </template>
 
 <script>
+import firebaseApp from "../../firebase.js";
+import { getDocs, getFirestore } from "firebase/firestore";
+import { collection, doc } from "firebase/firestore";
+const db = getFirestore(firebaseApp);
+
 export default {
   data: () => ({
     dialog: false,
     dialogDelete: false,
     headers: [
-      {
-        title: "S/N",
-        align: "center",
-        key: "sn",
-      },
+      { title: "S/N", key: "sn",  align: "center" },
       { title: "Date (DD/MM/YY)", key: "date", align: "center" },
       { title: "Time", key: "time", align: "center" },
       { title: "Teleconsult", key: "teleconsult", align: "center" },
-      { title: "Actions", key: "actions", sortable: false, align: "center" },
+      { title: "Actions", key: "actions", sortable: false, align: "center" }
     ],
-    data: [],
+    data: [{
+          sn: 1,
+          date: "13/05/24",
+          time: "17:30",
+          teleconsult: "NIL",
+          MC: "NIL",
+        }],
   }),
 
   created() {
@@ -36,7 +43,12 @@ export default {
   },
 
   methods: {
-    initialize() {
+    async initialize() {
+      const querySnapshot = await getDocs(collection(db, "davidyeong555@gmail.com"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+      });
       this.data = [
         {
           sn: 1,
@@ -44,6 +56,7 @@ export default {
           time: "17:30",
           teleconsult: "NIL",
           MC: "NIL",
+        
         },
       ];
     },
