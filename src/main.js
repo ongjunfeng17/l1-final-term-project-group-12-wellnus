52737032b33674298381964d954c0e39d4035192
lingreading <wouldlikesomebanter@gmail.com>
2024-04-02 18:48:17 +0800

    Configure tailwind

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import App from "./App.vue";
 import router from "./router/index.js";
 
 // Vuetify
-import 'vuetify/dist/vuetify.min.css';
+import "vuetify/dist/vuetify.min.css";
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 import { mdiDelete, mdiGoogle } from "@mdi/js";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+
+// tailwind
+import "./index.css";
 
 const vuetify = createVuetify({
   icons: {
