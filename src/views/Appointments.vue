2a8a6b89f8cd883886407ee849a71761b974b8dc
YZDavid <85034436+YZDavid@users.noreply.github.com>
2024-04-07 00:07:29 +0800

    add userRole to differentiate between patient and doctor on appointments and form page

--- src/views/Appointments.vue
+++ src/views/Appointments.vue
@@ -4,7 +4,8 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import PatientAppointmentTable from "@/components/tables/PatientAppointmentTable.vue";
 import DoctorAppointmentTable from "@/components/tables/DoctorAppointmentTable.vue";
 import PatientPastApptTable from "@/components/tables/PatientPastApptTable.vue";
-import { ref } from "vue";
+//import { ref } from "vue";
+import { getUserRole } from "../queries.js"
 
 export default {
   name: "Table",
@@ -13,31 +14,38 @@ export default {
     DoctorAppointmentTable,
     PatientPastApptTable
   },
-
+  /*
   setup() {
-    const userRole = ref("student");
+    const userRole = ref("patient");
     return { userRole }; // make `userRole` accessible in the template
   },
+  */
 
   data() {
     return {
       refreshComp: 0,
       user: null,
+      userRole: null
     };
   },
   methods: {
     change() {
       this.refreshComp += 1;
     },
+
+    async userSetup(user) {
+      if (!user) {
+        return;
+      }
+      this.user = user;
+      const userRole = await getUserRole(user.uid);
+      this.userRole = userRole;
+    }
   },
 
-  mounted() {
+  created() {
     const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        this.user = user;
-      }
-    });
+    onAuthStateChanged(auth, this.userSetup);
   },
 };
 </script>
@@ -45,7 +53,7 @@ export default {
 <template>
   <div v-if="user">
     Upcoming Appointments
-    <v-container v-if="userRole === 'student'">
+    <v-container v-if="userRole === 'patient'">
       <PatientAppointmentTable />
     </v-container>
 
@@ -54,7 +62,7 @@ export default {
     </v-container>
 
     Past Appointments
-    <v-container v-if="userRole === 'student'">
+    <v-container v-if="userRole === 'patient'">
       <PatientPastApptTable />
     </v-container>
 
